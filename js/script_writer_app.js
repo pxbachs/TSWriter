b95ba14201e1e9f2d1059490e191135da3e4ccd9
Bach Pham <pxbachs@gmail.com>
2016-03-03 08:42:58 +0700

    use angular dnd

--- js/script_writer_app.js
+++ js/script_writer_app.js
@@ -1,27 +1,5 @@
-var scriptWriterApp = angular.module('scriptWriterApp', []);
+var scriptWriterApp = angular.module('scriptWriterApp', ['ui.bootstrap', "dndLists"]);
 
 function createSortableCloneList(elm) {
-	Sortable.create(elm, {
-				group : {
-					name : 'shared',
-					pull : 'clone',
-					put : false
-				},
-				onEnd : function(/**Event*/evt) {
-					evt.oldIndex// element's old index within parent
-					evt.newIndex// element's new index within parent
 
-					//console.log(el.innerText);
-
-					var el = evt.item;
-		            console.log(el.parentElement.className);
-		            if(el.parentElement.className == 'list-group sidebarsorttable') {
-		            	console.log(el.parentElement.id);
-		                var menu = document.getElementById(el.parentElement.id);
-		                menu.removeChild(menu.lastElementChild);
-		                menu.getElementsByTagName("LI")[evt.oldIndex].style.display = '';
-		            }
-				},
-				sort : false //Disabling sorting
-			});
 }
--- js/script_writer_ctroller.js
+++ js/script_writer_ctroller.js
@@ -1,11 +1,121 @@
-scriptWriterApp.controller('StepsController', function($scope, $http) {
+scriptWriterApp.controller('StepsController', ["$scope", "$http", function($scope, $http) {
 	$http.get('data/ios/steps_defination.json').success(function(data) {
+		//$scope.group_steps = [];
+		//$scope.group_steps.push({pre_def: data});
+		//console.log($scope.group_steps);
 		$scope.steps_def = data;
 		angular.forEach($scope.steps_def, function(value, key){
               console.log(value.id + ": " + key);
          });
+         
+         $scope.models = [];
+         $scope.models.feature = [{"A":{}}];
 	});
-}); 
+}]); 
+
+scriptWriterApp.controller('FeatureController', ["$scope", "$http", "$uibModal", "$log", function($scope, $http, $uibModal, $log) {
+	
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+  
+  
+	
+	$scope.index_id = 0;
+	
+	$http.get('data/ios/steps_defination.json').success(function(data) {
+		//$scope.group_steps = [];
+		//$scope.group_steps.push({pre_def: data});
+		//console.log($scope.group_steps);
+		$scope.steps_def = [{"id": "see", "name": "See", allowedTypes: ['step'], "type":"scenario", "steps": []}];
+		
+		angular.forEach($scope.steps_def, function(value, key){
+              //console.log(value.id + ": " + key);
+         });
+
+	});
+	
+	$scope.scenarioAllowedTypes= ["scenario"];
+	$scope.stepAllowedTypes= ["step"];
+	
+	$scope.models = {
+        selected: null,
+        templates: [
+            {index: 1, type: "step",name:"Step", id: "custom-",  syntax:"Then You want an action here"},
+            {index: 1, id: "scenario_",  name: "Scenario ", allowedTypes: ['step'], type:"scenario", steps: []}
+        ]
+      };
+      
+	$scope.addScenario = function() {
+		//console.log($scope.steps_def);
+		$scope.index_id = $scope.index_id + 1;
+		$scope.steps_def = $scope.steps_def.concat({"id": "scenari0_" + $scope.index_id,  "name": "Scenario " + $scope.index_id, allowedTypes: ['step'], "type":"scenario", "steps": []});
+		//$scope.models.dropzones["A"] = $scope.models.dropzones["A"].concat({type: "container", id: 10, columns: [[], []]});
+	};
+	
+	$scope.showEdit = function(evt){
+		console.log(evt.step);
+		$('#exampleModal').modal({});
+		
+	};
+	
+	$scope.$watch('steps_def', function(model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+        features = "";
+        angular.forEach($scope.steps_def, function(value, key){
+              //console.log(value.id + ": " + value.steps);
+              features += "Scenario: " + value.name;
+              angular.forEach(value.steps, function(value, key){
+	              //console.log(value.id + ": " + value.syntax);
+	              features += "\n" + value.syntax;
+         		});
+         	features += "\n\n\n";
+         });
+        
+        $scope.features = features;
+    }, true);
+}]);
+
+scriptWriterApp.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
 
 scriptWriterApp.directive('stepsRepeatDirective', function() {
   return function(scope, element, attrs) {
